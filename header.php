564acf4969c29c1c89f5ee4a0e5fe49f2c37fa26
Marek Grabowski <grabowskispace@gmail.com>
2018-03-26 23:43:01 +0200

    0.1.5
    Logo

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 		<!-- Pasek górnego menu -->
 		<nav class="navbar navbar-dark navbar-expand-md bg-primary">
 			<div class="container">
-				<a href="index.php" class="navbar-brand">Studio Dentico</a>
+				<a href="index.php" class="navbar-brand"><img src="images/logo.png" alt="logo"></a>
 				<!-- Przycisk menu aktywny na małych urzdzeniach -->
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navContent">
 					<span class="navbar-toggler-icon"></span>
